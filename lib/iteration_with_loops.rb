b2a906c29ff4f70f228a60232bb4d4d4c74e0f9b
Nicholas Ho <nah1222@yahoo.com>
2020-08-11 18:36:12 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -6,7 +6,7 @@ def join_nested_strings(mixed_data)
   num = 0
   while num < mixed_data.length do
   element_index = 0
-  while element_index < mixed_data[num].count do
+  while element_index < mixed_data[num].length do
     if mixed_data[num][element_index].is_a?(String)
       word = mixed_data[num][element_index]
       str << word
